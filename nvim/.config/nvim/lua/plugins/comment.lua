7997edb19002406059f0c4b977197f07ef0be7ef
Suresh Saragadam <saragadamsuresh@gmail.com>
2025-11-28 23:40:09 +0530

    Arch config perfect working dual monitor setup --no led backlight --neovim ready

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,7 +1,10 @@
 local sw = require("config.switch")
 
 return {
-	"numToStr/Comment.nvim",
+	"echasnovski/mini.comment",
+	version = false,
+	config = true,
+
 	enabled = sw.comment,
 	opts = {},
 	lazy = false, -- so it's available immediately
